af40f45ea886ebfadf6e6565e6bf741a597d53e3
unknown <christiandc19@hotmail.com>
2022-04-12 06:48:07 -0700

    changed ssequelize.sync({ force: true}) to false

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(session(sess));
 // turn on routes
 app.use(routes);
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
